22745ec5b56259656e21521c798b573d30f67eb6
Mario-Code7 <marioroa71@yahoo.com>
2025-12-01 15:08:26 -0600

    sql connection done

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -5,8 +5,8 @@ import java.sql.*;
 public class Program {
     public static void main(String[] args) throws SQLException {
         String url = "jdbc:mysql://localhost:3306/sakila";
-        String username = "root";
-        String password = "";
+        String username = args[0];
+        String password = args[1];
 
 
         Connection connection;
